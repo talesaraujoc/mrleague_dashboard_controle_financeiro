c7749aa4c992da065324080dca2602ac2647c8f9
Tales Araújo Carvalho <tales.araujocarv@hotmail.com>
2023-08-16 16:17:10 -0300

    row_3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import dash, html, dcc, Output, Input, dash_table
+from dash import dash, html, dcc, ctx, Output, Input, dash_table
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 
@@ -25,8 +25,8 @@ from globals import *
 card_receita = dbc.Card(
     dbc.CardBody(
         [
-            html.H5("Receitas", className="card-title"),
-            html.H3(id='receita-valor')
+            html.H6("Receitas", className="card-title"),
+            html.H4(id='receita-valor')
         ]
     ),
     style={}
@@ -35,8 +35,8 @@ card_receita = dbc.Card(
 card_despesa= dbc.Card(
     dbc.CardBody(
         [
-            html.H5("Despesas", className="card-title"),
-            html.H3(id='despesa-valor')
+            html.H6("Despesas", className="card-title"),
+            html.H4(id='despesa-valor')
         ]
     ),
     style={}
@@ -45,8 +45,8 @@ card_despesa= dbc.Card(
 card_resultado_mes= dbc.Card(
     dbc.CardBody(
         [
-            html.H5("Balanço (=)", className="card-title"),
-            html.H3(id='balanco-valor')
+            html.H6("Balanço (=)", className="card-title"),
+            html.H4(id='balanco-valor')
         ]
     ),
     style={}
@@ -57,9 +57,14 @@ card_resultado_mes= dbc.Card(
 app.layout = html.Div([
         dbc.Row(dcc.Dropdown(options=lista_meses, value=lista_meses[-1], id='disparador-geral-meses')),
         
-        dbc.Row([dbc.Col([card_resultado_mes], lg=4), dbc.Col([card_receita], lg=4), dbc.Col([card_despesa], lg=4)]),
+        dbc.Row([dbc.Col([card_receita], lg=4), dbc.Col([card_despesa], lg=4), dbc.Col([card_resultado_mes], lg=4)]),
         
-        dbc.Row()
+        dbc.Row([
+                dbc.Col(dcc.Dropdown(options=lista_drop_esquerda, value=lista_drop_esquerda[0], id='dpd-01-r3/c1'), lg=6), 
+                dbc.Col([dcc.Dropdown(id='dpd-02-r3/c2')], lg=6)
+                ]),
+        
+        dbc.Row([dbc.Col([dcc.Graph(id='grafico-01-r4/c1')], lg=8), dbc.Col([], lg=4)])
 ])
 
 
@@ -70,11 +75,11 @@ app.layout = html.Div([
 )
 def update_receita(mes):
     if mes == 'Ano':
-        valor = df_receitas['VALOR'].sum()
+        valor = df_receitas['VALOR_RECEITA'].sum()
         return valor
     else:
         df_receita_mes = df_receitas.loc[df_receitas['COMPETÊNCIA']==mes]
-        valor = df_receita_mes['VALOR'].sum()
+        valor = df_receita_mes['VALOR_RECEITA'].sum()
         return valor
 
 
@@ -84,11 +89,11 @@ def update_receita(mes):
 )
 def update_despesa(mes):
     if mes == 'Ano':
-        valor = df_despesas['VALOR'].sum()
+        valor = df_despesas['VALOR_DESPESA'].sum()
         return valor
     else:
         df_despesa_mes = df_despesas.loc[df_despesas['COMPETÊNCIA']==mes]
-        valor = df_despesa_mes['VALOR'].sum()
+        valor = df_despesa_mes['VALOR_DESPESA'].sum()
         return valor
     
 
@@ -98,21 +103,57 @@ def update_despesa(mes):
 )
 def update_balanco(mes):
     if mes == 'Ano':
-        valor_receita = df_receitas['VALOR'].sum()
-        valor_despesa = df_despesas['VALOR'].sum()
+        valor_receita = df_receitas['VALOR_RECEITA'].sum()
+        valor_despesa = df_despesas['VALOR_DESPESA'].sum()
         valor_final = valor_receita - valor_despesa
         return valor_final
     else:
         df_receita_m = df_receitas.loc[df_receitas['COMPETÊNCIA']==mes]
-        x_receita = df_receita_m['VALOR'].sum()
+        x_receita = df_receita_m['VALOR_RECEITA'].sum()
         
         df_despesa_m = df_despesas.loc[df_despesas['COMPETÊNCIA']==mes]
-        x_despesa = df_despesa_m['VALOR'].sum()
+        x_despesa = df_despesa_m['VALOR_DESPESA'].sum()
         
         y_final = x_receita - x_despesa
         return y_final
         
-        
+@app.callback(
+    Output('dpd-02-r3/c2','options'),
+    Input('dpd-01-r3/c1', 'value')
+)
+def update_dropdown_function(value):
+    if value == 'Análise':
+        return lista_filtragem_categoria
+    else:
+        return lista_tipo_fluxo_caixa
+    
+@app.callback(
+    Output('dpd-02-r3/c2', 'value'),
+    Input('dpd-02-r3/c2', 'options')
+)
+def set_drop(set):
+    return set[0]
+
+@app.callback(
+    Output('grafico-01-r4/c1', 'figure'),
+    Input('dpd-01-r3/c1', 'value'),
+    Input('dpd-02-r3/c2', 'value')
+)
+def update_grafico_01(parametro_esq, parametro_dir):
+    if parametro_esq == 'Análise':
+        fig = go.Figure()
+        return fig
+    else:
+        if parametro_dir == 'Fluxo caixa mes/mes':
+            fig = make_subplots(rows=1, cols=1)
+
+            fig.add_trace(go.Bar(x=df_receitas_graph_01.index, y=df_receitas_graph_01['VALOR_RECEITA'], showlegend=False, name='Receita'), row=1, col=1)
+
+            fig.add_trace(go.Bar(x=df_despesas_graph_01.index, y=df_despesas_graph_01['VALOR_DESPESA'], showlegend=False, name='Despesa'), row=1, col=1)
+            return fig
+        else:
+            fig_year_cashflow = go.Figure(data=go.Bar(x=df_year_cashflow.index, y=df_year_cashflow['CASH_FLOW']))
+            return fig_year_cashflow
 # Servidor
 if __name__=='__main__':
     app.run_server(debug=True)
--- globals.py
+++ globals.py
@@ -4,9 +4,24 @@ import pandas as pd
 df_receitas = pd.read_excel('data/receitas_2022.xlsx')
 df_despesas = pd.read_excel('data/despesas_2022.xlsx')
 
+#primeira alteração dataset
+df_receitas.rename(columns={"VALOR": "VALOR_RECEITA"}, inplace=True)
+df_despesas.rename(columns={"VALOR":'VALOR_DESPESA'}, inplace=True)
 
 
+#fluxo caixa anual, dataframes:
+df_receitas_graph_01 = df_receitas.groupby('COMPETÊNCIA', sort=False).agg({'VALOR_RECEITA':'sum'}).reset_index()
+df_despesas_graph_01 = df_despesas.groupby('COMPETÊNCIA', sort=False).agg({'VALOR_DESPESA':'sum'}).reset_index()
+
+#dataframe year_cashflow
+df_year_cashflow = pd.merge(df_receitas_graph_01, df_despesas_graph_01, how='inner',on='COMPETÊNCIA')
+df_year_cashflow['CASH_FLOW'] = df_year_cashflow['VALOR_RECEITA']-df_year_cashflow['VALOR_DESPESA']
 
 # lista meses
 lista_meses = df_receitas['COMPETÊNCIA'].unique().tolist()
 lista_meses.append('Ano')
+
+# lista opções gráficos
+lista_drop_esquerda = ['Análise', 'Fluxo de Caixa']
+lista_filtragem_categoria = ['Receitas por categoria', 'Despesas por Categoria']
+lista_tipo_fluxo_caixa = ['Fluxo caixa mes/mes', 'Cashflow ano']
